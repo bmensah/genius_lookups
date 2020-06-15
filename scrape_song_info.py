01d7696776b308bc7e3d594e4a51f03fcbfcc709
Bradley Mensah <b.men16@gmail.com>
2020-06-15 13:18:21 -0400

    Used 'unidecode' module to properly format strings
    accented characters were causing issues in the URLs, so I used unidecode to change any letters with accents to the closest non-accented character

--- scrape_song_info.py
+++ scrape_song_info.py
@@ -2,6 +2,7 @@ import requests
 import html
 from bs4 import BeautifulSoup
 from unicodedata import normalize
+import unidecode
 
 
 def scrape_song_info(url) -> list:
@@ -10,16 +11,18 @@ def scrape_song_info(url) -> list:
     soup = BeautifulSoup(page.text, 'html.parser')
     title = html.unescape(str(soup.title))
 
-    # get artist name substring, strip off whitespace, and normalize any unicode chars
-    start = title.find('<title>') + len('<title>')
-    end = title.find(chr(8211))
-    artist_name = normalize('NFKD', title[start:end].strip())
-
     # get song name substring, strip off whitespace, and normalize any unicode chars
     start = title.find(chr(8211)) + len(chr(8211))
     end = title.find("Lyrics |")
     song_name = normalize('NFKD', title[start:end].strip())
+    song_name = unidecode.unidecode(song_name)
+
+    # get artist name substring, strip off whitespace, and normalize any unicode chars
+    start = title.find('<title>') + len('<title>')
+    end = title.find(chr(8211))
+    artist_name = normalize('NFKD', title[start:end].strip())
+    artist_name = unidecode.unidecode(artist_name)
 
     # add artist name and song name to a list, return list
-    info = [artist_name, song_name]
+    info = [song_name, artist_name]
     return info
